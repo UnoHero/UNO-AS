d272a5d9c5539ced21ae72c013609057ff08730b
Alpha <scottunogundersen@gmail.com>
2024-07-03 14:33:40 +0200

    Cleanup of react and start

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './index.css';
+
+// Components
+import Header from './components/Header';
+
+// Pages
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>UNO AS</h1>
+        </div>
+    );
+}
+ 
+export default Header;

d0f5199277d673b6b096016f9dc5972782edb528
Alpha <scottunogundersen@gmail.com>
2024-07-03 15:03:04 +0200

    Created Img folder, added more to header, made css and links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,18 @@
+import React from 'react';
+
+import Logo from "../img/unoAsLogo.png";
+
 const Header = () => {
     return (
-        <div>
-            <h1>UNO AS</h1>
-        </div>
+        <header className="header">
+            <img src={Logo} alt="UNO AS LOGO" className="logo" />
+            <nav className="nav">
+                <a href="" className="nav-link">Tjenester</a>
+                <a href="" className="nav-link">Kontakt oss</a>
+                <a href="" className="nav-link">Ring oss: +47...</a>
+            </nav>
+        </header>
     );
 }
- 
+
 export default Header;
